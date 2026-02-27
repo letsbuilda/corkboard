8c5fcade2dcc19efbc08b69d4b4c3f3d8632b44b
maxillum <gdcolon555@gmail.com>
2026-02-27 23:06:26 +0000

    this will take a while to develop

--- src/main.py
+++ src/main.py
@@ -1,2 +1,20 @@
-def main() -> None: ...
-main()
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "You've found an instance of corkboard, a single-file image hosting service built with FastAPI!"}
+
+@app.get("/{id}")
+async def show(img_id):
+    # fetch image and send it to the user as raw data
+    return {"success": img_id}
+
+@app.post("/upload")
+async def upload(img_data):
+    # upload picture to server and create id
+    # this wasnt implemented because its almost 11 PM
+    return {"id": uploaded.id}
+
+# welp i'll see you in a few days, that's my entire commit for now.
